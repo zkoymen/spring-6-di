0b56daaef420d94d9523d3edab16f3e71ab47368
zkoymen <zeynepkoymen9@gmail.com>
2025-06-24 17:34:03 +0300

    new changes

---
+++ src/main/java/zeyzey/spring_6_di/Spring6DiApplication.java
@@ -0,0 +1,13 @@
+package zeyzey.spring_6_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiApplication.class, args);
+	}
+
+}
