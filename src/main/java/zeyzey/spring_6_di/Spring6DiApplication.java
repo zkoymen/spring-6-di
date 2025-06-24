7df827a3454a02f3243b3ea42275f5242a8fcca4
zkoymen <zeynepkoymen9@gmail.com>
2025-06-24 17:58:28 +0300

    Created the sample controller

--- src/main/java/zeyzey/spring_6_di/Spring6DiApplication.java
+++ src/main/java/zeyzey/spring_6_di/Spring6DiApplication.java
@@ -2,12 +2,20 @@ package zeyzey.spring_6_di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import zeyzey.spring_6_di.controllers.MyController;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In main method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
